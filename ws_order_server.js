ccdb3572ca58e45146f0488515fea917069a9350
Daniel Dihardja <daniel.dihardja@socian.net>
2013-10-31 09:39:40 +0100

    created a second server which uses the websocket class

--- ws_order_server.js
+++ ws_order_server.js
@@ -4,7 +4,7 @@ function WSOrderServer() {
 	
 	this.commandHandler = {};
 	
-	this.run = function(port, onOpenHandler) {
+	this.run = function(port) {
 		
 		var WebSocketServer = require('websocket').server;
 		var http = require('http');	
@@ -30,23 +30,26 @@ function WSOrderServer() {
 		});
 		
 		
-		wsServer.on('request', function(message) {
+		wsServer.on('request', function(request) {
 			if(! _this.originIsAllowed(request.origin)) {
 				request.reject();
 				console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
 				return;
 			}
 			
-			var connection = request.accept('echo-protocol', request.origin);
+			var connection = request.accept('', request.origin);
 			console.log((new Date()) + ' Connection accepted.');
 			
 			connection.on('message', function(message) {
 				if (message.type === 'utf8') {
+					console.log(message.utf8Data);
 					var msg = JSON.parse(message.utf8Data);
 					var handler = _this.commandHandler[msg.command];
+						
 					if(handler != null) {
 						handler.apply(handler, [connection, msg.data]);
 					}
+					
 				}
 			});
 		});
@@ -62,6 +65,22 @@ function WSOrderServer() {
 }
 
 
+function OrderModel() {
+	var _this = this;
+	this.data = {
+		orderlist:{}
+	}
+	
+	this.orderListArray = function() {
+		var arr = new Array();
+		for(var key in _this.data.orderlist) {
+			var item = _this.data.orderlist[key];
+			arr.push(item);
+		}
+		return arr;
+	}
+}
+
 //------------------//
 //		Usage
 //------------------//
@@ -70,16 +89,116 @@ var s = new WSOrderServer();
 
 var staffConnection = null;
 var orderConnections = {};
+var orderTable = {};
+var orderId = 0;
 
 s.onCommand('REGISTER_STAFF', function(connection, data) {
 	staffConnection = connection;
 });
+
 s.onCommand('NEW_ORDER', function(connection, data) {
+	console.log('NEW_ORDER');
+	data.status = 1;
+	data.orderid = "order_" + orderId;
+	orderTable[ data.orderid ] = data;
+	orderId ++;
+	var msg = {command:'ORDER_UPDATE', data:data}
+	connection.sendUTF(JSON.stringify(msg));
+	
+	orderConnections[data.orderid] = connection;
+	
+	if(staffConnection == null) return;
+	
+	// notify the staff that there is a new order
+	
+	// TODO:
+	// outsource into a model
+	var orderArr = new Array();
+	for(var key in orderTable) {
+		var item = orderTable[key];
+		orderArr.push(item);
+	}
+	var msg = {command:'ORDER_UPDATE', data:orderArr}
+	staffConnection.sendUTF(JSON.stringify(msg));
 	
 });
+
+s.onCommand('GET_ORDER', function(connection, data) {
+	console.log('GET_ORDER');
+	
+	var oid = data.orderid;
+	var order = orderTable[oid];
+	
+	var msg = {command:'ORDER_UPDATE', data:order}
+	connection.sendUTF(JSON.stringify(msg));
+});
+	
 s.onCommand('GET_ORDER_LIST', function(connection, data) {
+	console.log('GET_ORDER_LIST');
+	var orderArr = new Array();
 	
+	// TODO:
+	// outsource into a model
+	for(var key in orderTable) {
+		var item = orderTable[key];
+		orderArr.push(item);
+	}
+	var msg = {command:'ORDER_UPDATE', data:orderArr}
+	connection.sendUTF(JSON.stringify(msg));
+});	
+
+s.onCommand('UPDATE_ORDER_STATUS_READY', function(connection, data) {
+	console.log('UPDATE_ORDER_STATUS_READY');
+	var oid = data.orderid;
+	var order = orderTable[oid];
+	order.status = 2;
+	orderTable[oid] = order;
+	var guestCon = orderConnections[oid];
+	var msg2guest = {command:'ORDER_UPDATE', data:order}
+	guestCon.sendUTF(JSON.stringify(msg2guest));
+	
+	// TODO:
+	// outsource into a model
+	var orderArr = new Array();
+	for(var key in orderTable) {
+		var item = orderTable[key];
+		orderArr.push(item);
+	}
+	
+	var msg2staff = {command:'ORDER_UPDATE', data:orderArr}
+	connection.sendUTF(JSON.stringify(msg2staff));
 });
+
+s.onCommand('UPDATE_ORDER_STATUS_COMPLETE', function(connection, data) {
+	console.log('UPDATE_ORDER_STATUS_COMPLETE');
+	var oid = data.orderid;
+	delete orderTable[oid];
 	
+	var emptyOrder = {
+		items : [],
+		status : 0,
+		total : 0,
+		orderid: ''
+	}
+	
+	var msg2guest = {command:'ORDER_UPDATE', data:emptyOrder}
+	var guestCon = orderConnections[oid];
+	guestCon.sendUTF(JSON.stringify(msg2guest));
+	
+	// does this also delete orderConnections[oid] ? 
+	//delete guestCon;
+	
+	// TODO:
+	// outsource into a model
+	var orderArr = new Array();
+	for(var key in orderTable) {
+		var item = orderTable[key];
+		orderArr.push(item);
+	}
+	
+	var msg2staff = {command:'ORDER_UPDATE', data:orderArr}
+	connection.sendUTF(JSON.stringify(msg2staff));
+});
+
 // start the server
 s.run( 8080 );

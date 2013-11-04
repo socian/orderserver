87d9c650acbbddcfb82e76b2557e11b79641f384
Daniel Dihardja <daniel.dihardja@socian.net>
2013-11-04 10:03:23 +0100

    refactoring

--- mobile_order_server.js
+++ mobile_order_server.js
@@ -1,69 +1,21 @@
-function WSOrderServer() {
-	
-	var _this = this;
-	
-	this.commandHandler = {};
-	
-	this.run = function(port) {
-		
-		var WebSocketServer = require('websocket').server;
-		var http = require('http');	
-		
-		var server = http.createServer(function(request, response) {
-    		console.log((new Date()) + ' Received request for ' + request.url);
-    		response.writeHead(404);
-    		response.end();
-		});
-		
-		server.listen(port, function() {
-   			console.log((new Date()) + ' Server is listening on port 8080');
-		});
-		
-		var wsServer = new WebSocketServer({
-    		httpServer: server,
-    		// You should not use autoAcceptConnections for production
-    		// applications, as it defeats all standard cross-origin protection
-    		// facilities built into the protocol and the browser.  You should
-    		// *always* verify the connection's origin and decide whether or not
-    		// to accept it.
-    		autoAcceptConnections: false
-		});
-		
-		
-		wsServer.on('request', function(request) {
-			if(! _this.originIsAllowed(request.origin)) {
-				request.reject();
-				console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-				return;
-			}
-			
-			var connection = request.accept('', request.origin);
-			console.log((new Date()) + ' Connection accepted.');
-			
-			connection.on('message', function(message) {
-				if (message.type === 'utf8') {
-					console.log(message.utf8Data);
-					var msg = JSON.parse(message.utf8Data);
-					var handler = _this.commandHandler[msg.command];
-						
-					if(handler != null) {
-						handler.apply(handler, [connection, msg.data]);
-					}
-					
-				}
-			});
-		});
-	}
-	
-	this.onCommand = function(command, handler) {
-		_this.commandHandler[command] = handler;
-	}
-	
-	this.originIsAllowed = function(origin) {
-		return true;
-	}
+//--------------------------------------------------------//
+//      Commands for communicating with the client
+//--------------------------------------------------------//
+
+var OrderCommand = {
+	REGISTER_STAFF : "registerStaff",
+	NEW_ORDER : "newOrder",
+	GET_ORDER : "getOrder",
+	GET_ORDER_LIST : "getOrderList",
+	UPDATE_ORDER_STATUS_READY : "updateOrderStatusReady",
+	UPDATE_ORDER_STATUS_COMPLETE : "updateOrderStatusComplete",
+	ORDER_UPDATE : "orderUpdate"
 }
 
+
+//---------------------------//
+//      Order Model Class
+//---------------------------//
 
 function OrderModel() {
 	var _this = this;
@@ -83,12 +35,14 @@ function OrderModel() {
 	
 }
 
-//------------------//
-//		Usage
-//------------------//
+//---------------------//
+//      Main Part
+//---------------------//
 
-var s = new WSOrderServer();
+// instantiate the web socket order server
+var orderServer = require('./wsserver').Instantiate();
 
+// instantiate the order model
 var orderModel = new OrderModel();
 
 var staffConnection = null;
@@ -96,11 +50,11 @@ var orderConnections = {};
 var orderTable = {};
 var orderId = 0;
 
-s.onCommand('REGISTER_STAFF', function(connection, data) {
+orderServer.onCommand('REGISTER_STAFF', function(connection, data) {
 	staffConnection = connection;
 });
 
-s.onCommand('NEW_ORDER', function(connection, data) {
+orderServer.onCommand('NEW_ORDER', function(connection, data) {
 	console.log('NEW_ORDER');
 	
 	var orderTable = orderModel.data.orderlist;
@@ -123,7 +77,7 @@ s.onCommand('NEW_ORDER', function(connection, data) {
 	
 });
 
-s.onCommand('GET_ORDER', function(connection, data) {
+orderServer.onCommand('GET_ORDER', function(connection, data) {
 	console.log('GET_ORDER');
 	
 	var orderTable = orderModel.data.orderlist;
@@ -135,7 +89,7 @@ s.onCommand('GET_ORDER', function(connection, data) {
 	connection.sendUTF(JSON.stringify(msg));
 });
 	
-s.onCommand('GET_ORDER_LIST', function(connection, data) {
+orderServer.onCommand('GET_ORDER_LIST', function(connection, data) {
 	console.log('GET_ORDER_LIST');
 	
 	var orderArr = orderModel.orderListArray();
@@ -143,7 +97,7 @@ s.onCommand('GET_ORDER_LIST', function(connection, data) {
 	connection.sendUTF(JSON.stringify(msg));
 });	
 
-s.onCommand('UPDATE_ORDER_STATUS_READY', function(connection, data) {
+orderServer.onCommand('UPDATE_ORDER_STATUS_READY', function(connection, data) {
 	console.log('UPDATE_ORDER_STATUS_READY');
 	
 	var orderTable = orderModel.data.orderlist;
@@ -162,7 +116,7 @@ s.onCommand('UPDATE_ORDER_STATUS_READY', function(connection, data) {
 	connection.sendUTF(JSON.stringify(msg2staff));
 });
 
-s.onCommand('UPDATE_ORDER_STATUS_COMPLETE', function(connection, data) {
+orderServer.onCommand('UPDATE_ORDER_STATUS_COMPLETE', function(connection, data) {
 	console.log('UPDATE_ORDER_STATUS_COMPLETE');
 	var oid = data.orderid;
 	delete orderModel.data.orderlist[oid];
@@ -187,4 +141,4 @@ s.onCommand('UPDATE_ORDER_STATUS_COMPLETE', function(connection, data) {
 });
 
 // start the server
-s.run( 8080 );
+orderServer.run( 8080 );
---
+++ orderserver.js
@@ -0,0 +1,73 @@
+//------------------------//
+// Order Server Class
+//------------------------//
+
+OrderServer = function() {
+	
+	var _this = this;
+	
+	this.commandHandler = {};
+	
+	this.run = function(port) {
+		
+		var WebSocketServer = require('websocket').server;
+		var http = require('http');	
+		
+		var server = http.createServer(function(request, response) {
+    		console.log((new Date()) + ' Received request for ' + request.url);
+    		response.writeHead(404);
+    		response.end();
+		});
+		
+		server.listen(port, function() {
+   			console.log((new Date()) + ' Server is listening on port 8080');
+		});
+		
+		var wsServer = new WebSocketServer({
+    		httpServer: server,
+    		// You should not use autoAcceptConnections for production
+    		// applications, as it defeats all standard cross-origin protection
+    		// facilities built into the protocol and the browser.  You should
+    		// *always* verify the connection's origin and decide whether or not
+    		// to accept it.
+    		autoAcceptConnections: false
+		});
+		
+		
+		wsServer.on('request', function(request) {
+			if(! _this.originIsAllowed(request.origin)) {
+				request.reject();
+				console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+				return;
+			}
+			
+			var connection = request.accept('', request.origin);
+			console.log((new Date()) + ' Connection accepted.');
+			
+			connection.on('message', function(message) {
+				if (message.type === 'utf8') {
+					console.log(message.utf8Data);
+					var msg = JSON.parse(message.utf8Data);
+					var handler = _this.commandHandler[msg.command];
+						
+					if(handler != null) {
+						handler.apply(handler, [connection, msg.data]);
+					}
+					
+				}
+			});
+		});
+	}
+	
+	this.onCommand = function(command, handler) {
+		_this.commandHandler[command] = handler;
+	}
+	
+	this.originIsAllowed = function(origin) {
+		return true;
+	}
+}
+
+exports.instantiate = function() {
+	return new OrderServer();
+}
